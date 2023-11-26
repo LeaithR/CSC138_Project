db554a2bc30e675527ce8e77fdb8e64324a7e036
Fadi Zubeideh <zubeidehfadi@gmail.com>
2023-11-25 22:28:36 -0800

    fix: commands now work when inputted

--- client.py
+++ client.py
@@ -1,6 +1,22 @@
 # client code CSC 138 Project
 import socket
- 
+import threading
+
+# function to handle user input in a separate thread
+def handle_user_input(client_socket, username):
+    while True:
+        # Get user input
+        user_input = input(f"{username}: ")
+
+        # Send the user input to the server
+        client_socket.send(user_input.encode())
+
+        # Check if the user wants to exit
+        if user_input.lower().strip() == 'bye':
+            break
+
+    # Close the client socket after the 'bye' command is received        
+    client_socket.close()
  
 def clientFunc():
     # port number and address
@@ -18,16 +34,18 @@ def clientFunc():
   
      # connect to server
     client.connect((clientHostName, portNum))
-  
+
+    user_input_thread = threading.Thread(target=handle_user_input, args=(client, userName))
+    user_input_thread.start()
+
     message = input(str(userName) + ": ")
   
-     # send and recieve messages
-    while message.lower().strip() != 'bye':
-         client.send(message.encode())
-         data = client.recv(1024).decode()
-  
-         print(data)
-         message = input(str(userName) + ": ")
+    # receive and display messages from the server
+    while True:
+        data = client.recv(1024).decode()
+        if not data:
+            break
+        print(data)
   
     client.close()
   
